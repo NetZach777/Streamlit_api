adf481d2d7d80cfc1738e85b2c86441082bafb32
NetZach777 <150202716+NetZach777@users.noreply.github.com>
2025-03-24 04:47:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import streamlit as st
 from typing import Generator
 from groq import Groq
+from PIL import Image
+import time
 
 st.set_page_config(page_icon="💬", layout="wide", page_title="Nessem_Projet")
 
@@ -16,48 +18,44 @@ icon("🐉")
 
 st.subheader("Beta test Kingdom_IA")
 
-# Utilisation de la clé API à partir des variables d'environnement
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 
-# Validation de la clé API avant d'initialiser le client
 if GROQ_API_KEY is None:
     st.error("Clé API Groq manquante. Vérifiez vos variables d'environnement.")
 else:
     client = Groq(api_key=GROQ_API_KEY)
 
-    # Initialiser l'historique des messages et le modèle sélectionné
     if "messages" not in st.session_state:
         st.session_state.messages = []
-
     if "selected_model" not in st.session_state:
         st.session_state.selected_model = None
-
-    # Définir les détails des modèles avec les données fournies
+    
+ # Définir les détails des modèles avec les données fournies
     models = {
-        "gemma2-9b-it": {"name": "Gemma 2 9B", "tokens": 8192, "developer": "Google"},
-        "llama-3.3-70b-versatile": {"name": "Llama 3.3 70B Versatile", "tokens": 32768, "developer": "Meta"},
-        "llama-3.1-8b-instant": {"name": "Llama 3.1 8B Instant", "tokens": 8192, "developer": "Meta"},
-        "llama-guard-3-8b": {"name": "Llama Guard 3 8B", "tokens": 8192, "developer": "Meta"},
-        "llama3-70b-8192": {"name": "Meta Llama 3 70B", "tokens": 8192, "developer": "Meta"},
-        "llama3-8b-8192": {"name": "Meta Llama 3 8B", "tokens": 8192, "developer": "Meta"},
-        "mixtral-8x7b-32768": {"name": "Mixtral 8x7B", "tokens": 32768, "developer": "Mistral"},
-        "whisper-large-v3": {"name": "Whisper Large V3", "tokens": 25000, "developer": "OpenAI"},
-        "whisper-large-v3-turbo": {"name": "Whisper Large V3 Turbo", "tokens": 25000, "developer": "OpenAI"},
-        "qwen-qwq-32b": {"name": "Qwen-QWQ 32B", "tokens": 128000, "developer": "Alibaba Cloud"},
-        "mistral-saba-24b": {"name": "Mistral Saba 24B", "tokens": 32000, "developer": "Mistral"},
-        "qwen-2.5-coder-32b": {"name": "Qwen 2.5 Coder 32B", "tokens": 128000, "developer": "Alibaba Cloud"},
-        "qwen-2.5-32b": {"name": "Qwen 2.5 32B", "tokens": 128000, "developer": "Alibaba Cloud"},
-        "deepseek-r1-distill-qwen-32b": {"name": "DeepSeek Distill Qwen 32B", "tokens": 16384, "developer": "DeepSeek"},
-        "deepseek-r1-distill-llama-70b-specdec": {"name": "DeepSeek Distill Llama 70B SpecDec", "tokens": 16384, "developer": "DeepSeek"},
-        "deepseek-r1-distill-llama-70b": {"name": "DeepSeek Distill Llama 70B", "tokens": 128000, "developer": "DeepSeek"},
-        "llama-3.3-70b-specdec": {"name": "Llama 3.3 70B SpecDec", "tokens": 8192, "developer": "Meta"},
-        "llama-3.2-1b-preview": {"name": "Llama 3.2 1B Preview", "tokens": 8192, "developer": "Meta"},
-        "llama-3.2-3b-preview": {"name": "Llama 3.2 3B Preview", "tokens": 8192, "developer": "Meta"},
-        "llama-3.2-11b-vision-preview": {"name": "Llama 3.2 11B Vision Preview", "tokens": 8192, "developer": "Meta"},
-        "llama-3.2-90b-vision-preview": {"name": "Llama 3.2 90B Vision Preview", "tokens": 8192, "developer": "Meta"}
-    }
-
-    # Disposition pour la sélection du modèle et le curseur max_tokens
+    # Modèles de traitement du langage naturel (NLP)
+    "gemma2-9b-it": {"name": "Gemma 2 9B", "tokens": 8192, "developer": "Google", "type": "chat"},
+    "meta-llama-3.3-70b-versatile": {"name": "Llama 3.3 70B Versatile", "tokens": 32768, "developer": "Meta", "type": "chat"},
+    "meta-llama-3.1-8b-instant": {"name": "Llama 3.1 8B Instant", "tokens": 8192, "developer": "Meta", "type": "chat"},
+    "meta-llama-guard-3-8b": {"name": "Llama Guard 3 8B", "tokens": 8192, "developer": "Meta", "type": "chat"},
+    "meta-llama-3-70b": {"name": "Meta Llama 3 70B", "tokens": 8192, "developer": "Meta", "type": "chat"},
+    "meta-llama-3-8b": {"name": "Meta Llama 3 8B", "tokens": 8192, "developer": "Meta", "type": "chat"},
+    "mistral-8x7b": {"name": "Mixtral 8x7B", "tokens": 32768, "developer": "Mistral", "type": "chat"},
+    "qwen-qwq-32b": {"name": "Qwen-QWQ 32B", "tokens": 128000, "developer": "Alibaba Cloud", "type": "chat"},
+    "mistral-saba-24b": {"name": "Mistral Saba 24B", "tokens": 32000, "developer": "Mistral", "type": "chat"},
+    "qwen-2.5-coder-32b": {"name": "Qwen 2.5 Coder 32B", "tokens": 128000, "developer": "Alibaba Cloud", "type": "chat"},
+    "qwen-2.5-32b": {"name": "Qwen 2.5 32B", "tokens": 128000, "developer": "Alibaba Cloud", "type": "chat"},
+    "deepseek-r1-distill-qwen-32b": {"name": "DeepSeek Distill Qwen 32B", "tokens": 16384, "developer": "DeepSeek", "type": "chat"},
+    "deepseek-r1-distill-llama-70b": {"name": "DeepSeek Distill Llama 70B", "tokens": 128000, "developer": "DeepSeek", "type": "chat"},
+    
+    # Modèles de vision
+    "meta-llama-3.2-11b-vision-preview": {"name": "Llama 3.2 11B Vision Preview", "tokens": 8192, "developer": "Meta", "type": "vision"},
+    "meta-llama-3.2-90b-vision-preview": {"name": "Llama 3.2 90B Vision Preview", "tokens": 8192, "developer": "Meta", "type": "vision"},
+    
+    # Modèles de reconnaissance vocale
+    "openai-whisper-large-v3": {"name": "Whisper Large V3", "tokens": 25000, "developer": "OpenAI", "type": "audio"},
+    "openai-whisper-large-v3-turbo": {"name": "Whisper Large V3 Turbo", "tokens": 25000, "developer": "OpenAI", "type": "audio"}
+}
+    
     col1, col2 = st.columns(2)
 
     with col1:
@@ -65,10 +63,9 @@ else:
             "Choisissez un modèle :",
             options=list(models.keys()),
             format_func=lambda x: f"{models[x]['name']} - {models[x]['tokens']} tokens ({models[x]['developer']})",
-            index=0  # Défaut à Distil-Whisper English
+            index=0
         )
 
-    # Détecter le changement de modèle et vider l'historique des messages si le modèle a changé
     if st.session_state.selected_model != model_option:
         st.session_state.messages = []
         st.session_state.selected_model = model_option
@@ -76,58 +73,50 @@ else:
     max_tokens_range = models[model_option]["tokens"]
 
     with col2:
-        # Ajuster le curseur max_tokens dynamiquement en fonction du modèle sélectionné
         max_tokens = st.slider(
             "Max Tokens :",
-            min_value=512,  # Valeur minimum pour permettre une certaine flexibilité
+            min_value=512,
             max_value=max_tokens_range,
-            value=min(8000, max_tokens_range),  # Valeur par défaut ou maximum autorisé si moins
-            step=512,
-            help=f"Ajustez le nombre de tokens pour la réponse du modèle. Max pour {models[model_option]['name']} : {max_tokens_range}"
+            value=min(8000, max_tokens_range),
+            step=512
         )
 
-    # Affichage des informations supplémentaires sur le modèle
     st.write(f"Modèle sélectionné : **{models[model_option]['name']}**")
     st.write(f"Développeur : {models[model_option]['developer']}")
     st.write(f"Nombre maximal de tokens : {models[model_option]['tokens']}")
 
-    # Avertissement pour les modèles en prévisualisation
-    preview_models = [
-        "qwen-qwq-32b", "mistral-saba-24b", "qwen-2.5-coder-32b", 
-        "qwen-2.5-32b", "deepseek-r1-distill-qwen-32b", "deepseek-r1-distill-llama-70b-specdec",
-        "deepseek-r1-distill-llama-70b", "llama-3.3-70b-specdec", 
-        "llama-3.2-1b-preview", "llama-3.2-3b-preview", "llama-3.2-11b-vision-preview", 
-        "llama-3.2-90b-vision-preview"
-    ]
-    if model_option in preview_models:
-        st.warning("Ce modèle est en prévisualisation et peut ne pas être stable.")
+    if models[model_option]["type"] == "vision":
+        uploaded_image = st.file_uploader("Upload une image", type=["png", "jpg", "jpeg"])
+    else:
+        uploaded_image = None
 
-    # Option pour effacer l'historique des messages
     if st.button("Effacer l'historique"):
         st.session_state.messages = []
 
-    # Afficher les messages de chat de l'historique lors de la réexécution de l'application
     for message in st.session_state.messages:
-        avatar = '🐉' if message["role"] == "assistant" else '👨‍👩‍👧‍👧'
+        avatar = '🐉' if message["role"] == "assistant" else '👤'
         with st.chat_message(message["role"], avatar=avatar):
             st.markdown(message["content"])
 
     def generate_chat_responses(chat_completion) -> Generator[str, None, None]:
-        """Génère le contenu de la réponse du chat à partir de la réponse de l'API Groq."""
         full_content = ""
         for chunk in chat_completion:
             if chunk.choices[0].delta.content:
                 full_content += chunk.choices[0].delta.content
+                yield chunk.choices[0].delta.content
         yield full_content
 
-    # Gestion des entrées utilisateur et génération de réponses
-    if prompt := st.chat_input("Entrez votre message ici..."):
+    if prompt := st.chat_input("Entrez votre message ici...") or uploaded_image:
+        if uploaded_image:
+            image = Image.open(uploaded_image)
+            st.image(image, caption="Image téléchargée", use_column_width=True)
+            prompt = "Analyse cette image."
+
         st.session_state.messages.append({"role": "user", "content": prompt})
 
         with st.chat_message("user", avatar='👤'):
             st.markdown(prompt)
 
-        # Obtenir la réponse de l'API Groq
         try:
             with st.spinner('Génération de la réponse...'):
                 chat_completion = client.chat.completions.create(
@@ -139,17 +128,17 @@ else:
                     max_tokens=max_tokens,
                     stream=True
                 )
-
-                # Utiliser la fonction génératrice avec st.write_stream
+                
                 full_response = ""
                 with st.chat_message("assistant", avatar="👽"):
                     for chunk in generate_chat_responses(chat_completion):
                         full_response += chunk
                         st.markdown(chunk)
-                
-                # Ajouter la réponse complète à l'historique des messages
+                        time.sleep(0.05)
+
                 st.session_state.messages.append(
-                    {"role": "assistant", "content": full_response})
+                    {"role": "assistant", "content": full_response}
+                )
 
         except client.AuthenticationError:
             st.error("Erreur d'authentification. Vérifiez votre clé API.", icon="🚨")
